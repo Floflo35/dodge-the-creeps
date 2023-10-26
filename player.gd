e351edb480c93c80f8455513a7f6c86c3e266dd8
Florent Chardevel <fchardevel@gmail.com>
2023-10-26 17:34:34 +0200

    Coding player + enemy

--- player.gd
+++ player.gd
@@ -1,10 +1,12 @@
 extends Area2D
+signal hit
 
 @export var speed = 400 # Vitesse de déplacement du joueur (pixels/sec).
 var screen_size # Taille de l'écran du jeu.
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 	
 func _process(delta):
 	var velocity = Vector2.ZERO # Vecteur de mouvement du joueur.
@@ -25,3 +27,23 @@ func _process(delta):
 		
 	position += velocity * delta # Modifier la position du joueur.
 	position = position.clamp(Vector2.ZERO, screen_size) # Restreindre la position du joueur à la taille de l'écran.
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+func _on_body_entered(body):
+	hide() # Le player disparaît lorsqu'il est touché.
+	hit.emit()
+	#$CollisionShape2D.disabled = true
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$COllisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
