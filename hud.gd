e2292eeec91dfa68012ecf788ab90af9ca2c5a11
Florent Chardevel <fchardevel@gmail.com>
2024-01-22 11:25:17 +0100

    gitignore does not work

--- hud.gd
+++ hud.gd
@@ -3,10 +3,12 @@ extends CanvasLayer
 # Notifies "Main" node that the button has been pressed
 signal start_game
 
+
 func show_message(text):
 	$Message.text = text
 	$Message.show()
 	$MessageTimer.start()
+	
 	
 func show_game_over():
 	show_message("Game Over lol")
@@ -26,6 +28,7 @@ func _on_message_timer_timeout():
 func _on_start_button_pressed():
 	$StartButton.hide()
 	start_game.emit()
+	
 	
 func update_score(score):
 	$ScoreLabel.text = str(score)
